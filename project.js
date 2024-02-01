4ad993194511db24ca4b78081f99753dd89ba07a
elifery06 <elifery06@yandex.com>
2024-02-01 23:52:25 +0300

    first commit

---
+++ project.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini başlat
+const ui = new UI();
+
+//Tüm eventleri yükleme 
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addFilm);
+}
+
+function addFilm(e) {
+
+const title =titleElement.value;
+const director = directorElement.value;
+const url = urlElement.value;
+
+if(title === "" || director === "" || url === ""){
+    //Hata
+    ui.displayMessages("Tüm alanları doldurunuz...","danger");
+}
+else{
+    //Yeni film
+    const newFilm = new Film(title,director,url);
+
+    ui.addFilmToUI(newFilm); //Arayüze film ekleme
+    ui.displayMessages("Film başarıyla eklendi...","success");
+
+
+}
+ui.clearInputs(titleElement,urlElement,directorElement);//Inputları temizleme;
+
+    e.preventDefault();//
+}

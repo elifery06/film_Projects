e7b2fd25d2fca4287ae0659ac1366e150939a814
elifery06 <elifery06@yandex.com>
2024-02-06 15:37:11 +0300

    ES6 ile classlama

--- storage.js
+++ storage.js
@@ -1,43 +1,44 @@
-function Storage(){
+class Storage {
 
-}
+    static addFilmToStorage(newFilm){
+        let films = this.getFilmsFromStorage();
+    
+        films.push(newFilm);
+        localStorage.setItem("films",JSON.stringify(films));
 
-Storage.prototype.addFilmToStorage = function(newFilm){//Storage a film ekleme
-let films = this.getFilmsFromStorage();
-
-films.push(newFilm);//Arraya film ekleme
- 
-
-
-localStorage.setItem("films",JSON.stringify(films));//Stringe çevirme ve storage a yazma
-}
-
-Storage.prototype.getFilmsFromStorage = function(){
-
-    let films;
-
-if(localStorage.getItem("films") === null){
-    films = [];
-}
-else{
-    films = JSON.parse(localStorage.getItem("films"));
-
-}
-return films;
-}
-
-Storage.prototype.deleteFilmFromStorage = function(filmTitle){
-    let films = this.getFilmsFromStorage();
-
-    films.forEach(function(film,index){
-        if(film.title === filmTitle){
-            films.splice(index,1);
+    }
+    static getFilmsFromStorage(){
+        let films;
+    
+        if (localStorage.getItem("films") === null) {
+            films = [];
         }
-    });
+        else {
+            films = JSON.parse(localStorage.getItem("films"));
+    
+        }
+    
+        return films;
+    }
+    static deleteFilmFromStorage(filmTitle){
+        let films = this.getFilmsFromStorage();
+        // splice
+        films.forEach(function(film,index){
+            if(film.title === filmTitle) {
+                films.splice(index,1);
+            }
+        });
+    
+        localStorage.setItem("films",JSON.stringify(films));
+    
+    
+    
+    
+    }
+    static clearAllFilmsFromStorage(){
+    
+        localStorage.removeItem("films");
+        
+    }
 
-    localStorage.setItem("films",JSON.stringify(films));
-}
-
-Storage.prototype.clearAllFilmsFromStorage = function(){
-    localStorage.removeItem("films");
 }
